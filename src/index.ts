74c683e617c9d7fdac4a9114de4e4f6caa26b397
Mutefish0 <648262030@qq.com>
2020-04-26 15:22:09 +0800

    fix createSelector in nestetd cases

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Reducer, useEffect, useState as useReactState } from 'react';
 
-type Callback<T> = (arg1: T, arg2: T) => void;
+type Callback = () => any;
 function noop() {}
 
-interface Emitter<T> {
+interface Emitter {
   ids: number[];
-  callbackEntity: { [key: number]: Callback<T> };
+  callbackEntity: { [key: number]: Callback };
   digOn: typeof digOn;
   layOn: typeof layOn;
   off: typeof off;
@@ -13,53 +13,68 @@ interface Emitter<T> {
 }
 
 // 挖坑
-function digOn<T>(this: Emitter<T>, id: number) {
+function digOn(this: Emitter, id: number) {
   this.ids.push(id);
   this.callbackEntity[id] = noop;
 }
 // 躺坑
-function layOn<T>(this: Emitter<T>, id: number, cb: Callback<T>) {
+function layOn(this: Emitter, id: number, cb: Callback) {
   this.callbackEntity[id] = cb;
 }
 
-function off<T>(this: Emitter<T>, id: number) {
+function off(this: Emitter, id: number) {
   delete this.callbackEntity[id];
   this.ids = this.ids.filter((_id) => _id !== id);
 }
-function emit<T>(this: Emitter<T>, arg1: T, arg2: T) {
-  this.ids.forEach((id) => this.callbackEntity[id](arg1, arg2));
+function emit(this: Emitter) {
+  let updates: (() => void)[] = [];
+  for (let id of this.ids) {
+    const update = this.callbackEntity[id]();
+    if (update) {
+      updates.push(update);
+    }
+  }
+  updates.forEach((u) => u());
 }
-function createEmitter<T>(): Emitter<T> {
+function createEmitter(): Emitter {
   return { ids: [], callbackEntity: {}, digOn, layOn, off, emit };
 }
 
-interface Token {
+interface Token<MS> {
   id: number;
+  state: MS;
 }
 
 export default function SharedReducer<S, A>(reducer: Reducer<S, A>) {
   let store = reducer([][0], {} as A);
   let tokenId: number = 0;
   let batchMap: { [key: number]: boolean } = {};
-  const emitter = createEmitter<S>();
+  const emitter = createEmitter();
 
-  function useToken(): [Token, (t: Token) => void] {
-    const [cachedToken, setCachedToken] = useReactState<Token>({ id: tokenId + 1 });
+  function useToken<MS>(mapper: (s: S) => MS): [Token<MS>, (t: Token<MS>) => void] {
+    const [cachedToken, setCachedToken] = useReactState<Token<MS>>({
+      id: tokenId + 1,
+      state: undefined as any,
+    });
     if (cachedToken.id > tokenId) {
       tokenId = cachedToken.id;
+      cachedToken.state = mapper(store);
       emitter.digOn(cachedToken.id);
     }
     batchMap[cachedToken.id] = true;
     return [cachedToken, setCachedToken];
   }
 
-  function mapState<T>(mapper: (state: S) => T): () => T {
-    return () => {
-      const [token, forceUpdate] = useToken();
+  function mapState<MS>(mapper: (state: S) => MS): () => MS {
+    return (notify?: () => void) => {
+      const [token, forceUpdate] = useToken<MS>(mapper);
       useEffect(() => {
-        emitter.layOn(token.id, (lastStore: S, nextStore: S) => {
-          if (!batchMap[token.id] && mapper(lastStore) !== mapper(nextStore)) {
-            forceUpdate({ id: token.id });
+        emitter.layOn(token.id, () => {
+          const nextState = mapper(store);
+          if (token.state !== nextState) {
+            token.state = nextState;
+            notify && notify();
+            return () => !batchMap[token.id] && forceUpdate({ ...token });
           }
         });
         return () => {
@@ -67,15 +82,14 @@ export default function SharedReducer<S, A>(reducer: Reducer<S, A>) {
         };
       }, []);
 
-      return mapper(store);
+      return token.state;
     };
   }
 
   function dispatch(action: A) {
-    const lastStore = store;
     store = reducer(store, action);
     batchMap = {};
-    emitter.emit(lastStore, store);
+    emitter.emit();
   }
 
   return [mapState, dispatch] as [typeof mapState, typeof dispatch];
@@ -85,60 +99,60 @@ export default function SharedReducer<S, A>(reducer: Reducer<S, A>) {
 // 当且仅当依赖的状态发生改变，才会重新进行计算
 // 可以有效提升性能，同时避免出现死循环问题
 type ReturnArray<T> = {
-  [P in keyof T]: T[P] extends () => infer U ? () => U : () => T[P];
+  [P in keyof T]: T[P] extends (notify: () => void) => infer U
+    ? (notify: () => void) => U
+    : (notify: () => void) => T[P];
 }; // 变长参数类型推断
 type Args<T> = {
   [P in keyof T]: T[P] extends infer U ? U : T[P];
 }; // 变长参数类型推断
 export function createSelector<T0, T extends [any, any, any, any, any, any, any, any]>(
-  useStates: ReturnArray<T>,
+  useMappedStates: ReturnArray<T>,
   selector: (args: Args<T>) => T0
 ): () => T0;
 export function createSelector<T0, T extends [any, any, any, any, any, any, any]>(
-  useStates: ReturnArray<T>,
+  useMappedStates: ReturnArray<T>,
   selector: (args: Args<T>) => T0
 ): () => T0;
 export function createSelector<T0, T extends [any, any, any, any, any, any]>(
-  useStates: ReturnArray<T>,
+  useMappedStates: ReturnArray<T>,
   selector: (args: Args<T>) => T0
 ): () => T0;
 export function createSelector<T0, T extends [any, any, any, any, any]>(
-  useStates: ReturnArray<T>,
+  useMappedStates: ReturnArray<T>,
   selector: (args: Args<T>) => T0
 ): () => T0;
 export function createSelector<T0, T extends [any, any, any, any]>(
-  useStates: ReturnArray<T>,
+  useMappedStates: ReturnArray<T>,
   selector: (args: Args<T>) => T0
 ): () => T0;
 export function createSelector<T0, T extends [any, any, any]>(
-  useStates: ReturnArray<T>,
+  useMappedStates: ReturnArray<T>,
   selector: (args: Args<T>) => T0
 ): () => T0;
 export function createSelector<T0, T extends [any, any]>(
-  useStates: ReturnArray<T>,
+  useMappedStates: ReturnArray<T>,
   selector: (args: Args<T>) => T0
 ): () => T0;
 export function createSelector<T0, T extends [any]>(
-  useStates: ReturnArray<T>,
+  useMappedStates: ReturnArray<T>,
   selector: (args: Args<T>) => T0
 ): () => T0;
 export function createSelector<T0, T extends any[]>(
-  useStates: ReturnArray<T>,
+  useMappedStates: ReturnArray<T>,
   selector: (args: Args<T>) => T0
 ) {
   let cache: T0;
-  let cachedStates: T = [] as any;
-  return function () {
-    let useCache = true;
-    for (let i = 0; i < useStates.length; i++) {
-      const nextState = useStates[i]();
-      if (cachedStates[i] !== nextState) {
-        cachedStates[i] = nextState;
-        useCache = false;
-      }
+  let useCache = false;
+  return function (_notify: () => void) {
+    function notify() {
+      useCache = false;
+      _notify && _notify();
     }
+    const mappedStates = useMappedStates.map((u) => u(notify));
     if (!useCache) {
-      cache = selector(cachedStates as any);
+      cache = selector(mappedStates as Args<T>);
+      useCache = true;
     }
     return cache;
   };
---
+++ tests/createSelector/case4.test.tsx
@@ -0,0 +1,182 @@
+import React from 'react';
+import SharedReducer, { createSelector } from '../../src/index';
+import { render } from '@testing-library/react';
+
+import '../suppressWarn';
+
+interface State {
+  counterA: number;
+  counterB: number;
+  counterC: number;
+  counterD: number;
+}
+
+interface Action {
+  type: 'incareseA' | 'incareseB' | 'incareseC' | 'incareseD';
+}
+
+const initialState: State = { counterA: 0, counterB: 0, counterC: 0, counterD: 0 };
+
+const [mapState, dispatch] = SharedReducer((state: State = initialState, action: Action) => {
+  switch (action.type) {
+    case 'incareseA':
+      return { ...state, counterA: state.counterA + 1 };
+    case 'incareseB':
+      return { ...state, counterB: state.counterB + 1 };
+    case 'incareseC':
+      return { ...state, counterC: state.counterC + 1 };
+    case 'incareseD':
+      return { ...state, counterD: state.counterD + 1 };
+    default:
+      return state;
+  }
+});
+
+const useCounterA = mapState((x) => x.counterA);
+const useCounterB = mapState((x) => x.counterB);
+const useCounterC = mapState((x) => x.counterC);
+const useCounterD = mapState((x) => x.counterD);
+
+const useCounterAB = createSelector([useCounterA, useCounterB], ([counterA, counterB]) => {
+  return counterA + counterB;
+});
+
+const useCounterAB_C = createSelector([useCounterAB, useCounterC], ([counterAB, counterC]) => {
+  return counterAB + counterC;
+});
+
+const useCounterAB_D = createSelector([useCounterAB, useCounterD], ([counterAB, counterD]) => {
+  return counterAB + counterD;
+});
+
+const useCounterAB_C__AB_D = createSelector(
+  [useCounterAB_C, useCounterAB_D],
+  ([counterAB_C, counterAB_D]) => {
+    return counterAB_C + counterAB_D;
+  }
+);
+
+function incareseCounterA() {
+  dispatch({ type: 'incareseA' });
+}
+function incareseCounterB() {
+  dispatch({ type: 'incareseB' });
+}
+function incareseCounterC() {
+  dispatch({ type: 'incareseC' });
+}
+function incareseCounterD() {
+  dispatch({ type: 'incareseD' });
+}
+
+let renderingLogs = [];
+
+function A() {
+  const counterA = useCounterA();
+  renderingLogs.push({ component: 'A', counterA });
+  return <div></div>;
+}
+function B() {
+  const counterB = useCounterB();
+  renderingLogs.push({ component: 'B', counterB });
+  return <div></div>;
+}
+function C() {
+  const counterC = useCounterC();
+  renderingLogs.push({ component: 'C', counterC });
+  return <div></div>;
+}
+function D() {
+  const counterD = useCounterD();
+  renderingLogs.push({ component: 'D', counterD });
+  return <div></div>;
+}
+function AB() {
+  const counterAB = useCounterAB();
+  renderingLogs.push({ component: 'AB', counterAB });
+  return <div></div>;
+}
+function AB_C() {
+  const counterAB_C = useCounterAB_C();
+  renderingLogs.push({ component: 'AB_C', counterAB_C });
+  return <div></div>;
+}
+function AB_D() {
+  const counterAB_D = useCounterAB_D();
+  renderingLogs.push({ component: 'AB_D', counterAB_D });
+  return <div></div>;
+}
+
+function AB_C__AB_D() {
+  const counterAB_C__AB_D = useCounterAB_C__AB_D();
+  renderingLogs.push({ component: 'AB_C__AB_D', counterAB_C__AB_D });
+  return <div></div>;
+}
+
+function App() {
+  return (
+    <div>
+      <A />
+      <B />
+      <C />
+      <D />
+      <AB />
+      <AB_C />
+      <AB_D />
+      <AB_C__AB_D />
+    </div>
+  );
+}
+
+describe('nested nested selector', () => {
+  test('works fine', () => {
+    renderingLogs = [];
+    render(<App />);
+    expect(renderingLogs).toEqual([
+      { component: 'A', counterA: 0 },
+      { component: 'B', counterB: 0 },
+      { component: 'C', counterC: 0 },
+      { component: 'D', counterD: 0 },
+      { component: 'AB', counterAB: 0 },
+      { component: 'AB_C', counterAB_C: 0 },
+      { component: 'AB_D', counterAB_D: 0 },
+      { component: 'AB_C__AB_D', counterAB_C__AB_D: 0 },
+    ]);
+
+    renderingLogs = [];
+    incareseCounterA();
+    expect(renderingLogs).toEqual([
+      { component: 'A', counterA: 1 },
+      { component: 'AB', counterAB: 1 },
+      { component: 'AB_C', counterAB_C: 1 },
+      { component: 'AB_D', counterAB_D: 1 },
+      { component: 'AB_C__AB_D', counterAB_C__AB_D: 2 },
+    ]);
+
+    renderingLogs = [];
+    incareseCounterB();
+    expect(renderingLogs).toEqual([
+      { component: 'B', counterB: 1 },
+      { component: 'AB', counterAB: 2 },
+      { component: 'AB_C', counterAB_C: 2 },
+      { component: 'AB_D', counterAB_D: 2 },
+      { component: 'AB_C__AB_D', counterAB_C__AB_D: 4 },
+    ]);
+
+    renderingLogs = [];
+    incareseCounterC();
+    expect(renderingLogs).toEqual([
+      { component: 'C', counterC: 1 },
+      { component: 'AB_C', counterAB_C: 3 },
+      { component: 'AB_C__AB_D', counterAB_C__AB_D: 5 },
+    ]);
+
+    renderingLogs = [];
+    incareseCounterD();
+    expect(renderingLogs).toEqual([
+      { component: 'D', counterD: 1 },
+      { component: 'AB_D', counterAB_D: 3 },
+      { component: 'AB_C__AB_D', counterAB_C__AB_D: 6 },
+    ]);
+  });
+});
